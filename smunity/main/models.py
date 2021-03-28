466c4a97cd9cbbeef8df63c6d69ebf788b1db434
rawheel <rawheeelalisiddiqui@gmail.com>
2021-03-28 17:50:55 +0500

    interest updated

--- smunity/main/models.py
+++ smunity/main/models.py
@@ -33,11 +33,13 @@ class User(AbstractUser):
     dob=models.DateField(max_length=8,null=True)
     city= models.CharField(max_length=50)
     profile_picture=models.ImageField(upload_to=upload_user_image,null=True,blank=True,default="defaultprofile.jpg")
-    interest=models.ManyToManyField("Interest",null=True,blank=True)
+    interest=models.ManyToManyField("Interest",blank=True)
 class Interest(models.Model):
     name=models.CharField(max_length=50)
-    description=models.CharField(max_length=500)
-    image=models.ImageField(upload_to='interests/')
+    #description=models.CharField(max_length=500)
+    #image=models.ImageField(upload_to='interests/')
+    def __str__(self):
+        return self.name
 
 class Category(models.Model):
     name=models.CharField(max_length=50)
