8527ac238fe1fea5adc50eaaf155909bcb6fd1b5
syedbilal28 <syed.bilal.sba@gmail.com>
2021-03-28 05:19:34 +0500

    User Creation Tested

--- smunity/main/admin.py
+++ smunity/main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import User,Community,Company
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Community)
+admin.site.register(Company)
